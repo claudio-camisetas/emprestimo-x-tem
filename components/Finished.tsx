a3251b31c1912d6aa25f78227fccc02d8bafa4f1
D-Freitas <davi.israel01@gmail.com>
2024-01-24 13:35:11 -0300

    refactor: change phone number

--- components/Finished.tsx
+++ components/Finished.tsx
@@ -9,7 +9,7 @@ import logo from '../images/logo.png'
 
 export default function Finished() {
   const openWhatsapp = () => {
-    location.href = 'https://api.whatsapp.com/send?phone=556796828216&text=Solicitar%20empr%C3%A9stimo!'
+    location.href = 'https://api.whatsapp.com/send?phone=5551995850105&text=Solicitar%20empr%C3%A9stimo!'
   }
 
   return (
